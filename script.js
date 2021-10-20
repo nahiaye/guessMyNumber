654d4641f6ec50deceef8895067da5d9a3c78169
Nahii <nahomayele7512@gmail.com>
2021-10-20 16:17:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,4 +60,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
+  
 });
